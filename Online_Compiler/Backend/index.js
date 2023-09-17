141e1b3d0f3f7a81fac58ccafffa4ed77c0aaea5
Betelgeuse997 <43148054+Betelgeuse997@users.noreply.github.com>
2023-09-17 20:49:12 +0530

    Upload_on_17-09-2023

--- Online_Compiler/Backend/index.js
+++ Online_Compiler/Backend/index.js
@@ -1,31 +1,81 @@
 const express = require('express');
 const { generateFile } = require('./generateFile');
-const { executeCpp } = require("./executeCpp");
+
+const {addJobToQueue} = require('./jobQueue')
+const Job = require("./models/Job");
 const cors = require("cors");
+const mongoose = require('mongoose');
+
+// mongoose.connect("mongodb://localhost/ojapp", (err) => {
+//     if(err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+//     console.log("Successfully connected to mongodb database");
+// });
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1/ojapp');
+  console.log("Successfully connected to mongodb database");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json({hello: "world!"});
+app.get("/status", async(req, res) => {
+    
+    
+    const jobId = req.query.id;
+    console.log("status requested", jobId);
+
+    if(jobId == undefined) {
+        return res.status(400).json({success: false, error: "missing id query param"});
+    }
+    try {
+        const job = await Job.findById(jobId);
+
+        if(job === undefined) {
+            return res.status(404).json({success: false, error: "invalid job id"});
+        }
+
+        return res.status(200).json({success: true, job});
+
+    }
+    catch (err) {
+        return res.status(400).json({success:false, error: JSON.stringify(err)});
+    }
+
 });
 
 app.post("/run", async (req, res) => {
     const {language = "cpp",code} = req.body;
-
+    
     if (code === undefined) {
         return res.status(400).json({success: false, error: "Empty code body"});
     }
+
+    let job;
+
     try {
     const filepath = await generateFile(language, code)
-    const output = await executeCpp(filepath);
-    return res.json({filepath, output});
+    job = await new Job({language, filepath}).save();
+    const jobId = job["_id"];
+    addJobToQueue(jobId);
+    console.log(job);
+    res.status(201).json({success: true, jobId});
+
+    } catch(err)
+    {
+        return res.status(500).json({success: false, err: JSON.stringify(err)})
     }
-    catch (err){
-        res.status(500).json({err})
-    }
+    
 });
 
 app.listen(5000, () => {
