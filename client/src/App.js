67d2c600a251da068ce65231c985089de5ca3181
Betelgeuse997 <43148054+Betelgeuse997@users.noreply.github.com>
2023-09-17 20:53:32 +0530

    upload_on_17-09-2023

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,118 @@
 
+
 import axios from "axios";
 import "./App.css";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import stubs from './defaultStubs';
+import moment from 'moment';
 
 function App() {
 
   const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("cpp");
   const [output, setOutput] = useState("");
+  const [status, setStatus] = useState("");
+  const [jobId, setJobId] = useState("");
+  const [jobDetails, setJobDetails] = useState(null);
+
+  useEffect(() => {
+    setCode(stubs[language])
+  }, [language]);
+
+  const renderTimeDetails = () => {
+    if(!jobDetails){
+      return "";
+    }
+    let result = '';
+    let {submittedAt, completedAt, startedAt} = jobDetails;
+    submittedAt = moment(submittedAt).toString()
+    result += `Submitted At: ${submittedAt}`
+    if(!completedAt || !startedAt) {
+      return result;
+    }
+    const start = moment(startedAt);
+    const end = moment(completedAt);
+    const executionTime = end.diff(start, 'seconds', true);
+    result += `Execution Time: ${executionTime}s`
+    return result;
+  }
 
   const handleSubmit = async () => {
     const payload = {
-      language: "cpp",
+      language,
       code
     };
     try {
-    const {data} = await axios.post("http://localhost:5000/run", payload)
-    setOutput(data.output);
+      setJobId("");
+      setStatus("");
+      setOutput("");
+      setJobDetails(null);
+    const {data} = await axios.post("http://localhost:5000/run", payload);
+    console.log(data);
+    setJobId(data.jobId);
+    let intervalId;
+
+    intervalId = setInterval(async () => 
+    
+     {
+    const {data: dataRes} = await axios.get(
+      "http://localhost:5000/status",
+      {
+        params: { id: data.jobId } }
+    );
+    const { success, job, error } = dataRes;
+    console.log(dataRes);
+
+    if(success){
+      const {status: jobStatus, output: jobOutput} = job;
+      setStatus(jobStatus);
+      setJobDetails(job);
+      if(jobStatus === "pending") return;
+      setOutput(jobOutput);
+      clearInterval(intervalId);
+    }else{
+      setStatus("Error: Please retry!");
+      console.error(error);
+      clearInterval(intervalId);
+      setOutput(error);
     }
-    catch (err)
+
+    console.log(dataRes);
+    }, 1000);
+    }
+    catch ({response})
     {
-      console.log(err.response);
+      if(response){
+        const errMsg = response.data.err.stderr;
+        setOutput(errMsg);
+      }
+      else{
+          setOutput("Error connecting to server!");
+      }
     }
   }
 
   return (
     <div className="App">
       <h1> Coders Arena</h1>
+      <div>
+        <label>Language: </label>
+        <select 
+          value={language}
+          onChange={(e) => {
+            let response = window.confirm(
+              "Any written code will be erased, if language is changed without submitting"
+            );
+            if (response) {
+              setLanguage(e.target.value);
+            }
+          }}
+        >
+          <option value="cpp">C++</option>
+          <option value="py">Python</option>
+        </select>
+      </div>
+      <br />
       <textarea 
       rows = "30" 
       cols = "90" 
@@ -38,6 +125,9 @@ function App() {
       <br />
       
       <button onClick={handleSubmit}>SUBMIT</button>
+      <p>{status}</p>
+      <p>{jobId && `JobID: ${jobId}`}</p>
+      <p>{renderTimeDetails()}</p>
       <p>{output}</p>
     </div>
   );
